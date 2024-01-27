097d6afe6020e1ea1c8ab992b7c7134a4d543bc6
Matthew <94463458+MatthewSowamber@users.noreply.github.com>
2024-01-27 12:29:52 +0000

    Click and Drag physics
    Gravity added

--- Assets/Scripts/ClickandDrag.cs
+++ Assets/Scripts/ClickandDrag.cs
@@ -6,13 +6,30 @@ public class ClickandDrag : MonoBehaviour
 {
     Vector2 difference = Vector2.zero;
 
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     private void OnMouseDown()
     {
         difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+        rb.gravityScale = 0;
+        print("a");
     }
 
     private void OnMouseDrag()
     {
         transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+        print("b");
+    }
+
+    private void OnMouseUp()
+    {
+        rb.gravityScale = 1;
+        rb.velocity = new Vector2 (0,0);
+        print("c");
     }
 }
